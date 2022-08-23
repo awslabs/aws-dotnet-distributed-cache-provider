27a5cc1fe72b517d24ea1833cc5466a9cee95f68
ShalomGottesman <41350511+ShalomGottesman@users.noreply.github.com>
2022-08-23 09:31:05 -0400

    Add util class to generate full name of calling class (#23)
    Add util class to generate full name of calling class and change table name definitions to call util class instead. Class ensures that table name used conforms to DynamoDB Table name constraints.

---
+++ test/AWS.DistributedCacheProviderIntegrationTests/IntegrationTestUtils.cs
@@ -0,0 +1,49 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace AWS.DistributedCacheProviderIntegrationTests
+{
+    public class IntegrationTestUtils
+    {
+        /// <summary>
+        /// Concatenates the calling method's namespace, class name, method name and the UTC timestamp in
+        /// milliseconds. To be used when generating a DynamoDB Table that needs a name that can be traced to which test
+        /// generated the table should it not be deleted in the test directly. The UTC timestamp is so that multiple
+        /// people can run the same test on the same account at the same time and hopefully not have a conflict.
+        /// </summary>
+        /// <returns>The calling methods information in "{namespace}-{class name}-{method name}-{UTC now}" format.</returns>
+        public static string GetFullTestName()
+        {
+            //This method is being called from Integration tests. The methods being used here are not returning null.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            var baseMethod = new StackTrace().GetFrame(1).GetMethod().ReflectedType;
+            var nameSpace = baseMethod.Namespace;
+            var className = baseMethod.DeclaringType.Name;
+            var methodName = baseMethod.Name.Split('<', '>')[1];
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            var fullName = $"{nameSpace}-{className}-{methodName}-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
+            //DynamoDB Table name cannot have special chars
+            var filteredName = Regex.Replace(fullName, @"[^0-9a-zA-Z]+", "");
+            //DynamoDB Table name length must be between 3 and 255 chars
+            //Check that is does not exceed 255
+            if(filteredName.Length > 255)
+            {
+                return filteredName.Substring(filteredName.Length - 255);
+            }
+            //DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() returns a string that is longer than 3 chars. No need to check that case.
+            else
+            {
+                return filteredName;
+            }
+        }
+    }
+}
